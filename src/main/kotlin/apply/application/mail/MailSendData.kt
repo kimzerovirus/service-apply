542c6f868ae38e8f7065c0340b2c57de512bc657
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-28 03:15:15 +0900

    refactor: 어노테이션 삭제

--- src/main/kotlin/apply/application/mail/MailSendData.kt
+++ src/main/kotlin/apply/application/mail/MailSendData.kt
@@ -1,12 +1,7 @@
 package apply.application.mail
 
-import com.sun.istack.NotNull
-
 data class MailSendData(
-    @field:NotNull
     val subject: String,
-    @field:NotNull
     val content: String,
-    @field:NotNull
     val targetMails: List<String>
 )
